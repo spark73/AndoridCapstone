8c2ee902ff346de8dcf968885ddd6c7304d701f7
bbarbe11 <46977782+bbarbe11@users.noreply.github.com>
2019-12-03 19:34:53 -0500

    Add files via upload

---
+++ 4155_android/4155/app/src/main/java/com/example/a4155/MapsActivity.kt
@@ -0,0 +1,46 @@
+package com.example.a4155
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a4155.R
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near UNCC.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val uncc = LatLng(35.3071,-80.731)
+        val fretwell =LatLng(35.3061011,-80.7291206)
+        mMap.addMarker(MarkerOptions().position(fretwell).title("Marker on Fretwell"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uncc, 15.0f))
+    }
+}
--- 4155_android/4155/app/src/main/java/com/example/a4155/ui/login/DelivSettings.kt
+++ 4155_android/4155/app/src/main/java/com/example/a4155/ui/login/DelivSettings.kt
@@ -1,7 +1,10 @@
 package com.example.a4155.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.a4155.MapsActivity
 import com.example.a4155.R
 
 class DelivSettings : AppCompatActivity() {
@@ -9,5 +12,11 @@ class DelivSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deliv_settings)
+
+        val directButton = findViewById<Button>(R.id.directButton)
+        directButton.setOnClickListener{
+            val intent = Intent(this@DelivSettings, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
